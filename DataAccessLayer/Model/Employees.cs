a62fdc94c213bf65b584b2a5c939d20b463c8ff1
mdraj <mdraj@DELL>
2023-02-23 12:11:06 +0530

    Add project files.

---
+++ DataAccessLayer/Model/Employees.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccessLayer.Model
+{
+    public class Employees
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string  Designation { get; set; }
+        public double Salary { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string  Qualification { get; set; }
+        public string Manager { get; set; }
+    }
+}
---
+++ DataAccessLayer/Model/LoginInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessLayer.Model
+{
+    public class LoginInfo
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
